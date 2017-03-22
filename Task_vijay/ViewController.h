cd2934868cbe67b9b81a523e1051b86c58cddfe3
codefrux <codefrux@codefruxs-mac.local>
2017-03-22 15:06:11 +0530

    Initial Commit

---
+++ Task_vijay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task_vijay
+//
+//  Created by codefrux on 22/03/17.
+//  Copyright © 2017 codefrux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Task_vijay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Task_vijay
+//
+//  Created by codefrux on 22/03/17.
+//  Copyright © 2017 codefrux. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
